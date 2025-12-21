6801c2048bfbb5b0acd8ed032562bb336fa5e43b
Maxime <maxime.rache@gmail.com>
2025-12-21 23:10:35 +0100

    test

--- src/components/Classes.tsx
+++ src/components/Classes.tsx
@@ -1,6 +1,6 @@
 import playersData from "../data/players.json"
 import classesData from "../data/classes.json"
-import type { DofusClasses, Player } from "../types/dofus";
+import type { DofusClasses, Player } from "../types/dofus"
 import PlayerCard from "./PlayerCard"
 
 
@@ -12,12 +12,13 @@ export default function Classes() {
 
     return (
 		<div className="block py-6 px-12 w-4/5 mx-auto">
-
-			<div className="flex flex-col gap-4">
-			{players.map(player => (
-				<PlayerCard key={player.name} playerInfo={player} classesInfo={classesInfo} />
-			))}
-			</div>
+			{players ? 
+				<div className="flex flex-col gap-4">
+					{players.map(player => (
+						<PlayerCard key={player.name} playerInfo={player} classesInfo={classesInfo} />
+					))}
+				</div>
+			: "erreur"}
 		</div>
     )
 }
--- src/components/TeamMaker.tsx
+++
@@ -1,8 +0,0 @@
-export default function TeamMaker() {
-
-	return (
-		<div>
-
-		</div>
-	)
-}
