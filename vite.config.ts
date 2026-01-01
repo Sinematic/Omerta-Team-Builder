7e21f178a214a9f2535d1ceb65cb2a91840cc9ce
Maxime <maxime.rache@gmail.com>
2026-01-01 20:46:07 +0100

    testing config fixes for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,17 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ], 
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  }
+	plugins: [
+		react(),
+		tailwindcss()
+	], 
+	resolve: {
+		alias: {
+		'@': path.resolve(__dirname, 'src'),
+		},
+	},  
+	build: {
+		outDir: 'dist'
+	},
+	base: '/'
 })
