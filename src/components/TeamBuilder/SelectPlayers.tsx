913b14cab4c8e2c0b7837f06da038f210550c09f
Maxime <maxime.rache@gmail.com>
2026-01-02 19:48:32 +0100

    adjustments in colors

--- src/components/TeamBuilder/SelectPlayers.tsx
+++ src/components/TeamBuilder/SelectPlayers.tsx
@@ -18,7 +18,7 @@ export default function SelectPlayers({ participants, handleClickOnParticipants,
         <h2 className="select-none text-center py-8 text-white">{message(participants.length)}</h2>
         <ul className="grid grid-cols-4 gap-2 max-w-xl mx-auto select-none">
             {players.map((player) => 
-                <li className={`px-4 py-2 text-center cursor-pointer ${participants.includes(player.name) ? " bg-amber-600 text-white" : "bg-white"}`} 
+                <li className={`px-4 py-2 text-center cursor-pointer ${participants.includes(player.name) ? " bg-amber-700 text-white" : "bg-stone-600 text-white"}`} 
                 key={player.name} onClick={() => handleClickOnParticipants(player.name)}>
                     {player.name}
                 </li> 
