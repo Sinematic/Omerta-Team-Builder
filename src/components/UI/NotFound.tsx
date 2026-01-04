6e38c5d99a9325caed487c71e5701e1b5fc9a4ff
Maxime <maxime.rache@gmail.com>
2026-01-04 15:08:25 +0100

    .

--- src/components/UI/NotFound.tsx
+++ src/components/UI/NotFound.tsx
@@ -1,7 +1,6 @@
-export default function NotFound({ message } : {message : string }) {
+export default function NotFound({ message } : { message : string }) {
 
     return (
-
 		<div className="grid place-items-center">
 			<h1 className="text-4xl font-semibold text-white py-[16%]"><span className="block text-center mb-4 text-7xl">404</span>{message}</h1>
 		</div>
