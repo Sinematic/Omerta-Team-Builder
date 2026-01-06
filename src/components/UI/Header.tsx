c8b99a4b2728dc7ee61473b35bb0f980cebfce60
Maxime <maxime.rache@gmail.com>
2026-01-06 16:59:33 +0100

    refactor completed for the header

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -18,25 +18,34 @@ export default function Header() {
     return (
         <nav className="bg-emerald-900 h-16 flex items-center px-8">
             <ul className="flex gap-48 justify-center w-full text-white font-semibold items-center">
-                <NavLink to="/picker" className={linksClasses}>
-                    Composition d'équipes
-                </NavLink>
 
-                <NavLink to="/classes-des-joueurs" className={linksClasses}>
-                    Classes des joueurs
-                </NavLink>
+                <li>
+                    <NavLink to="/picker" className={linksClasses}>
+                        Composition d'équipes
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/classes-des-joueurs" className={linksClasses}>
+                        Classes des joueurs
+                    </NavLink>
+                </li>
 
-                <NavLink to="/maps" className={linksClasses}>
-                    Maps
-                </NavLink>
+                <li>
+                    <NavLink to="/maps" className={linksClasses}>
+                        Maps
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/classement" className={linksClasses}>
+                        Classement
+                    </NavLink>
+                </li>
+                <li>
+                    <input type="text" className="bg-white rounded-lg text-black px-3 py-2 focus:outline-none focus:ring-0" 
+                    placeholder="Chercher un joueur" onChange={(e) => setPseudoSearched(e.target.value)} value={pseudoSearched} 
+                    onKeyDown={(e) => e.key === "Enter" ? search(pseudoSearched) : ""}/>
+                </li>
 
-                <NavLink to="/classement" className={linksClasses}>
-                    Classement
-                </NavLink>
-
-                <input type="text" className="bg-white rounded-lg text-black px-3 py-2 focus:outline-none focus:ring-0" 
-                placeholder="Chercher un joueur" onChange={(e) => setPseudoSearched(e.target.value)} value={pseudoSearched} 
-                onKeyDown={(e) => e.key === "Enter" ? search(pseudoSearched) : ""}/>
             </ul>
         </nav>
     )
